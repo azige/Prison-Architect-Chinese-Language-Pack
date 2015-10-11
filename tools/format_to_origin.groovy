ed1e357329a55504923e1d4d08136ab43d6739eb
Azige <zzb12@qq.com>
2015-10-11 20:23:11 +0800

    格式化资源文件

--- tools/format_to_origin.groovy
+++ tools/format_to_origin.groovy
@@ -1,41 +1,47 @@
 
-def origin = new File("../data/language/base-language-origin.txt")
-def target = new File("../data/language/base-language.txt")
+@groovy.transform.SourceURI sourceURI
+def baseDir = new File(sourceURI).parentFile
+def origin = new File(baseDir, "../data/language/base-language-origin.txt")
+def targets = [
+    new File(baseDir, "../data/language/base-language.txt"),
+    new File(baseDir, "../official-simplifiedchinese/base-language.txt")
+]
+targets.each{ target ->
+    def map = [:]
 
-def map = [:]
+    target.eachLine{
+        def str = it.trim()
+        if (str.size() < 1 || str[0] == '#'){
+            return
+        }
+        
+        def kv = str.split(/\s+/, 2)
+        if (kv.size() > 1){
+            map[kv[0]] = kv[1]
+        }else{
+            map[kv[0]] = "[TODO]"
+        }
+    }
 
-target.eachLine{
-    def str = it.trim()
-    if (str.size() < 1 || str[0] == '#'){
-        return
+    def sb = new StringBuilder()
+
+    origin.eachLine{
+        def str = it.trim()
+        if (str.size() < 1 || str[0] == '#'){
+            sb << it << '\n'
+            return
+        }
+        
+        def matcher = str =~ /(\w+)(\s+)(.+)/
+        def key = matcher[0][1]
+        def space = matcher[0][2]
+        def value = matcher[0][3]
+        if (map[key]){
+            sb << key << space << map[key] << '\n'
+        }else{
+            sb << key << space << '[TODO]' << value << '\n'
+        }
     }
-    
-    def kv = str.split(/\s+/, 2)
-    if (kv.size() > 1){
-        map[kv[0]] = kv[1]
-    }else{
-        map[kv[0]] = "[TODO]"
-    }
+
+    target.bytes = sb.toString().getBytes('UTF-8')
 }
-
-def sb = new StringBuilder()
-
-origin.eachLine{
-    def str = it.trim()
-    if (str.size() < 1 || str[0] == '#'){
-        sb << it << '\n'
-        return
-    }
-    
-    def matcher = str =~ /(\w+)(\s+)(.+)/
-    def key = matcher[0][1]
-    def space = matcher[0][2]
-    def value = matcher[0][3]
-    if (map[key]){
-        sb << key << space << map[key] << '\n'
-    }else{
-        sb << key << space << '[TODO]' << value << '\n'
-    }
-}
-
-target.bytes = sb.toString().getBytes('UTF-8')
